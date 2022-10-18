0386c01cb7befd9ac8d1c77d457fc9ca6c4d7962
kapadokia <ktnyandoch@gmail.com>
2022-10-19 00:50:44 +0300

    practised with different types of loops

---
+++ loops.rb
@@ -0,0 +1,24 @@
+i =4
+
+# # Loop Using While
+# while i<10
+#     puts "looping #{i}"
+#     i+=1
+# end
+
+# # Loop Using Counter
+# until i == 10
+#     puts "looping #{i}"
+#     i+=1
+# end
+
+# # Using times
+# 10.times do |i|
+#     puts "looping #{i}"
+# end
+
+
+# using each
+(0...20).each do |i|
+    puts "looping #{i}"
+end

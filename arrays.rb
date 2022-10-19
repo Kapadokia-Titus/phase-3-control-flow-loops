be2da673d01a0afbfb362ce3c158e8e0ac4f8c06
kapadokia <ktnyandoch@gmail.com>
2022-10-19 09:37:01 +0300

    loops and passes all the tests

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+require 'pry'
+my_harsh ={user1:"Kapadokia", user2:"Daisy", user3:"kristine"}
+
+
+user ={
+    name: "Henry",
+    address:{
+        street: "123 Fake Street",
+        city: "Queens",
+        state: "NY"
+    }
+}
+
+
+binding.pry
--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
+# happy new year countdown 
 def happy_new_year
-  # your code here
+  count =10
+  until count == 0
+    puts count
+    count -=1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +22,16 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+    (1..100).each do |i|
+      puts fizzbuzz(i)
+    end
 end
 
 def reverse_string(str)
-  # your code here
+  # empty stringholder
+  name = ""
+  str.length.times do |i|
+    name =str[i]+name
+  end
+  name
 end
